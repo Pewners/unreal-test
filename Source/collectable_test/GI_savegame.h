97f0a01816c56b110985a69c07d70610345d4b1d
Pewners <jfadikpe2021@elam.co.uk>
2023-10-23 11:54:05 +0100

    idk

---
+++ Source/collectable_test/GI_savegame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GI_savegame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COLLECTABLE_TEST_API UGI_savegame : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/collectable_test/collectable_test.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "collectable_test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, collectable_test, "collectable_test" );
